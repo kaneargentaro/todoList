8c09e91a5dcd6001cc75bd82986bb40287fa108b
Kane Argentaro <kane_argentaro@hotmail.com>
2025-03-09 15:39:24 +1100

    Adding env via Bun with Types

--- index.ts
+++ index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        return new Response("Bun!");
+    },
+});
+
+console.log('ENV VARIABLES:');
+console.log('===========================');
+console.log(`DB_HOST: ${process.env.DB_HOST}`);
+console.log(`DB_PORT: ${process.env.DB_PORT}`);
+console.log(`DB_USER: ${process.env.DB_USER}`);
+console.log(`DB_PASSWORD: ${process.env.DB_PASSWORD}`);
+console.log(`DB_URL: ${process.env.DB_URL}`);
+console.log('===========================');
+console.log();
+console.log(`Listening on http://localhost:${server.port} ...`);
+
